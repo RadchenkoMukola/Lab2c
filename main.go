9eaef29681ff7f92233260240f2613f213acc259
Radchenko Mukola <RadchenkoMukola@gmail.com>
2023-09-26 18:06:37 +0300

    update

--- main.go
+++ main.go
@@ -1,82 +1,67 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 	"math/rand"
-	"sync"
-	"time"
 )
 
-func main() {
-	rand.Seed(time.Now().UnixNano())
-
-	monastery1 := []int{10, 15, 8, 20}
-	monastery2 := []int{5, 25, 30, 12}
-
-	// Об'єднуємо список двох монастирів в один список.
-	energy := append(monastery1, monastery2...)
-
-	// Створюємо канал для обміну результатами між потоками.
-	resultChan := make(chan int)
-
-	// Створюємо WaitGroup для синхронізації горутин.
-	var wg sync.WaitGroup
-
-	// Створюємо список для зберігання індексів ченців.
-	indexes := make([]int, len(energy))
-	for i := range indexes {
-		indexes[i] = i
-	}
-
-	// Перемішуємо індекси ченців для рандомізації парування.
-	rand.Shuffle(len(indexes), func(i, j int) {
-		indexes[i], indexes[j] = indexes[j], indexes[i]
-	})
-
-	// Розпочинаємо боротьбу між ченцями в багатопоточному режимі.
-	for i := 0; i < len(indexes); i += 2 {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			index1, index2 := indexes[i], indexes[i+1]
-			energy1, energy2 := energy[index1], energy[index2]
-			winner := battle(energy1, energy2)
-			fmt.Printf("Бій між ченцем з енергією %d і ченцем з енергією %d. Переможець: ченец з енергією %d\n", energy1, energy2, winner)
-			resultChan <- winner
-		}(i)
-	}
-
-	// Закриваємо канал після завершення всіх боїв.
-	go func() {
-		wg.Wait()
-		close(resultChan)
-	}()
-
-	// Очікуємо результати боїв та визначаємо переможця в фінальному бою.
-	finalWinner := -1
-	for winner := range resultChan {
-		if finalWinner == -1 {
-			finalWinner = winner
-		} else {
-			finalWinner = battle(finalWinner, winner)
-		}
-	}
-
-	// Визначаємо, який монастир переміг на основі переможця фінального бою.
-	var winningMonastery string
-	if finalWinner <= len(monastery1) {
-		winningMonastery = "Гуань-Інь"
+func fight(a int, b int, o chan int) {
+	if rand.Intn(2) == 1 {
+		o <- b
 	} else {
-		winningMonastery = "Гуань-Янь"
+		o <- a
 	}
-
-	fmt.Printf("Переможець монастиря %s забирає статую боддісатви.\n", winningMonastery)
 }
 
-// Функція для визначення переможця між двома ченцями.
-func battle(energy1, energy2 int) int {
-	if energy1 > energy2 {
-		return energy1
+func main() {
+	l := list.New()
+	l.Init()
+
+	for i := 0; i < 32; i++ {
+		l.PushBack(0)
+		l.PushBack(1)
 	}
-	return energy2
+
+	round := 1
+
+	for l.Len() > 1 {
+		o := make(chan int)
+
+		val := l.Front()
+
+		fmt.Printf("Round %d:\n", round)
+
+		for val != nil {
+			go fight(val.Value.(int), val.Next().Value.(int), o)
+			val = val.Next().Next()
+		}
+
+		iterations := l.Len() / 2
+
+		l = list.New()
+		l.Init()
+
+		for i := 0; i < iterations; i++ {
+			winner := <-o
+			winnerName := "Гуань-Янь" // За замовчуванням вважаємо, що переміг Гуань-Янь (значення 0).
+
+			if winner == 1 {
+				winnerName = "Гуань-Інь" // Якщо переможець - 1, то він є Гуань-Інь.
+			}
+
+			fmt.Printf("   Fight %d: Переміг боєць монастирю %s\n", i+1, winnerName)
+			l.PushBack(winner)
+		}
+
+		round++
+	}
+
+	finalWinnerName := "Гуань-Янь" // За замовчуванням вважаємо, що фінальний переможець - Гуань-Янь (значення 0).
+
+	if l.Front().Value.(int) == 1 {
+		finalWinnerName = "Гуань-Інь" // Якщо фінальний переможець - 1, то він є Гуань-Інь.
+	}
+
+	fmt.Printf("Переможець монастирь %s забирає статую боддісатви.\n", finalWinnerName)
 }
